cbdaf30b971c92fdee95a671d2337eec89fdcbe0
Jupiter2359media <cao.vu@2359media.com.vn>
2016-02-15 17:49:10 +0700

    [FIX] Fix circle config.

--- prepare_solution.sh
+++ prepare_solution.sh
@@ -4,6 +4,7 @@
 # Then copy the ui test folder into solutions and include it into the settings.gradle
 #
 #
+export TARGET_REPO="https://github.com/jupitervn/Exercise1Solution.git"
 rm -rf solutions
 mkdir solutions
 git clone "$TARGET_REPO" solutions
@@ -11,11 +12,12 @@ echo "After clone"
 function add_permissions {
   local PERMISSION_LINE="<uses-permission android:name=\"android.permission.WRITE_EXTERNAL_STORAGE\"\/><uses-permission android:name=\"android.permission.READ_EXTERNAL_STORAGE\"\/><\/manifest>"
   echo "${PERMISSION_LINE}"
+  find . -not -path "*build/*" -name AndroidManifest.xml
   find . -not -path "*build/*" -name AndroidManifest.xml -exec sed -i '.bak' "s/<\/manifest>/${PERMISSION_LINE}/" {} \;
 }
-add_permissions
 cd solutions
 git checkout solution
+add_permissions
 rm -rf signup-uitest
 cp -r ../signup-uitest signup-uitest
 echo "include ':signup-uitest'" >> settings.gradle
